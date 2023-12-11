af1cbaa5835d0c82e9bcdfe8631b5a4a1a91fb66
Akshay Kumar <72661046+akshay8560@users.noreply.github.com>
2023-12-11 15:09:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) =>{
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_URL)
+mongoose.connect("mongodb+srv://user2000:test123@cluster0.rltppwq.mongodb.net/notes_db?retryWrites=true&w=majority")
 .then(()=>{
     app.listen(PORT, ()=>{
         console.log("Server started on port no. " + PORT);
